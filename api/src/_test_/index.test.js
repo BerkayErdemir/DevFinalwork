cfc914fd358e50a5f11483230e94976d2f80993f
Berkay Erdemir <berkay.erdemir@student.ehb.be>
2021-11-16 11:16:30 +0100

    added temp/tussentijds

--- api/src/_test_/index.test.js
+++ api/src/_test_/index.test.js
@@ -8,58 +8,37 @@ const {
 
 require('dotenv').config();
 const request = require('supertest');
-
-
 //-----------------------------------------------------------------
-
 test("Progress succesfully send", async () => {
-
     const response = await request(server).post("/players").send({
-
         username: "opop",
         password: "test",
         email: "testestes@hotmmayl.uk"
     });
     expect(response.statusCode).toBe(200);
-
-
 });
-
 test("Progress succesfully deleted", async () => {
-
     const response = await request(server).delete("/players/opop").send();
     expect(response.statusCode).toBe(200);
-
-
 });
 test("bad request 404", async () => {
-
     const response = await request(server).post("/udsdsdsds").send({
-
         progress: "chapter5"
     });
     expect(response.statusCode).toBe(404);
-
 });
-
 test("testing if the user can log in", async () => {
     const response = await request(server).post("/user/progress").send({
-
         name: "name",
         password: "password"
     });
     expect(response.body.UserId).toBeDefined;
-
-
 });
 
 test("deleting a user", async () => {
-
     const response = await request(server).delete("/users").send({
-
-
     });
-    expect(response.body.UserId);
+    expect(response.body.id);
 });
 
 // test("Delete name", async () => {
